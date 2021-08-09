564aec48138c62c14c87c43d980963fcaac5efca
y1xian <yyxnb@outlook.com>
2021-08-09 21:12:41 +0800

    添加application库

---
+++ utils/lib-application/consumer-rules.pro
@@ -0,0 +1,3 @@
+
+-dontwarn com.yyxnb.oh.application.**
+-keep class com.yyxnb.oh.application.** {*;}
--- utils/lib-application/src/main/java/com/yyxnb/oh/application/AppGlobals.java
+++ utils/lib-application/src/main/java/com/yyxnb/oh/application/AppGlobals.java
@@ -1,4 +1,4 @@
-package com.yyxnb.oh.app;
+package com.yyxnb.oh.application;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
