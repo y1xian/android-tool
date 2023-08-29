adb23cc27119e2c87b0c095dd86160c8bb95769d
y1xian <yyxnb@outlook.com>
2023-08-30 02:05:17 +0800

    update

--- modules/page/src/main/java/com/yyxnb/android/page/ContainerActivity.java
+++ modules/page/src/main/java/com/yyxnb/android/page/ContainerActivity.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.yyxnb.android.core.utils.BarUtil;
-import com.yyxnb.android.intent.SafeIntent;
+import com.yyxnb.android.secure.app.intent.SafeIntent;
 import com.yyxnb.android.skin.SkinResource;
 import com.yyxnb.android.skin.callback.OnSkinChangeCallback;
 import com.yyxnb.android.skin.support.SkinCompat;
@@ -30,90 +30,90 @@ import com.yyxnb.android.utils.LogUtil;
  */
 public class ContainerActivity extends AppCompatActivity implements OnSkinChangeCallback {
 
-	private static final String TAG = ContainerActivity.class.getSimpleName();
+    private static final String TAG = ContainerActivity.class.getSimpleName();
 
-	private SkinCompat mSkinCompat;
+    private SkinCompat mSkinCompat;
 
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		mSkinCompat = new SkinCompat(this, this);
-		super.onCreate(savedInstanceState);
-		BarUtil.setStatusBarTranslucent(getWindow(), true, false);
-		BarUtil.setStatusBarStyle(getWindow(), true);
-		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		FrameLayout mFrameLayout = new FrameLayout(this);
-		mFrameLayout.setId(android.R.id.content);
-		setContentView(mFrameLayout);
-		initView();
-	}
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        mSkinCompat = new SkinCompat(this, this);
+        super.onCreate(savedInstanceState);
+        BarUtil.setStatusBarTranslucent(getWindow(), true, false);
+        BarUtil.setStatusBarStyle(getWindow(), true);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        FrameLayout mFrameLayout = new FrameLayout(this);
+        mFrameLayout.setId(android.R.id.content);
+        setContentView(mFrameLayout);
+        initView();
+    }
 
-	@Override
-	public View onCreateView(View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
-		View view = mSkinCompat.createView(parent, name, context, attrs);
-		if (view == null) {
-			return super.onCreateView(parent, name, context, attrs);
-		}
-		return view;
-	}
+    @Override
+    public View onCreateView(View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        View view = mSkinCompat.createView(parent, name, context, attrs);
+        if (view == null) {
+            return super.onCreateView(parent, name, context, attrs);
+        }
+        return view;
+    }
 
-	protected Fragment initBaseFragment() {
-		return null;
-	}
+    protected Fragment initBaseFragment() {
+        return null;
+    }
 
-	public void initView() {
-		try {
-			SafeIntent intent = new SafeIntent(getIntent());
-			final Fragment mFragment = initBaseFragment();
-			if (mFragment != null) {
-				if (intent.getBundleExtra(ArgumentKeys.BUNDLE) != null) {
-					mFragment.setArguments(intent.getBundleExtra(ArgumentKeys.BUNDLE));
-				}
-				setRootFragment(mFragment, android.R.id.content);
-				return;
-			}
+    public void initView() {
+        try {
+            SafeIntent intent = new SafeIntent(getIntent());
+            final Fragment mFragment = initBaseFragment();
+            if (mFragment != null) {
+                if (intent.getBundleExtra(ArgumentKeys.BUNDLE) != null) {
+                    mFragment.setArguments(intent.getBundleExtra(ArgumentKeys.BUNDLE));
+                }
+                setRootFragment(mFragment, android.R.id.content);
+                return;
+            }
 
-			String fragmentName = intent.getStringExtra(ArgumentKeys.FRAGMENT);
-			if (fragmentName.isEmpty()) {
-				throw new IllegalArgumentException("can not find page fragmentName");
-			}
-			Class<?> fragmentClass = Class.forName(fragmentName);
-			Fragment fragment = (Fragment) fragmentClass.newInstance();
-			if (intent.getBundleExtra(ArgumentKeys.BUNDLE) != null) {
-				fragment.setArguments(intent.getBundleExtra(ArgumentKeys.BUNDLE));
-			}
-			setRootFragment(fragment, android.R.id.content);
-		} catch (Exception e) {
-			LogUtil.e(TAG, e.getMessage());
-		}
-	}
+            String fragmentName = intent.getStringExtra(ArgumentKeys.FRAGMENT);
+            if (fragmentName.isEmpty()) {
+                throw new IllegalArgumentException("can not find page fragmentName");
+            }
+            Class<?> fragmentClass = Class.forName(fragmentName);
+            Fragment fragment = (Fragment) fragmentClass.newInstance();
+            if (intent.getBundleExtra(ArgumentKeys.BUNDLE) != null) {
+                fragment.setArguments(intent.getBundleExtra(ArgumentKeys.BUNDLE));
+            }
+            setRootFragment(fragment, android.R.id.content);
+        } catch (Exception e) {
+            LogUtil.e(TAG, e.getMessage());
+        }
+    }
 
-	@Override
-	protected void onDestroy() {
-		// 释放内存
-		mSkinCompat.release(this);
-		mSkinCompat = null;
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        // 释放内存
+        mSkinCompat.release(this);
+        mSkinCompat = null;
+        super.onDestroy();
+    }
 
-	/**
-	 * 换肤的回调,用于扩展自定义View的换肤功能
-	 *
-	 * @param view         view
-	 * @param skinResource skinResource
-	 */
-	@Override
-	public void onSkinChange(View view, SkinResource skinResource) {
-	}
+    /**
+     * 换肤的回调,用于扩展自定义View的换肤功能
+     *
+     * @param view         view
+     * @param skinResource skinResource
+     */
+    @Override
+    public void onSkinChange(View view, SkinResource skinResource) {
+    }
 
-	public <T extends Fragment> void setRootFragment(T fragment, int containerId) {
-		try {
-			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-			transaction.replace(containerId, fragment, String.valueOf(fragment.hashCode()));
-			transaction.addToBackStack(String.valueOf(fragment.hashCode()));
-			transaction.commitAllowingStateLoss();
-		} catch (Exception e) {
-			LogUtil.e(TAG, e.getMessage());
-		}
-	}
+    public <T extends Fragment> void setRootFragment(T fragment, int containerId) {
+        try {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(containerId, fragment, String.valueOf(fragment.hashCode()));
+            transaction.addToBackStack(String.valueOf(fragment.hashCode()));
+            transaction.commitAllowingStateLoss();
+        } catch (Exception e) {
+            LogUtil.e(TAG, e.getMessage());
+        }
+    }
 }
--- modules/page/src/main/java/com/yyxnb/android/page/FragmentPageImpl.java
+++ modules/page/src/main/java/com/yyxnb/android/page/FragmentPageImpl.java
@@ -7,9 +7,9 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import com.yyxnb.android.core.utils.ActivityUtil;
-import com.yyxnb.android.intent.IntentUtils;
-import com.yyxnb.android.intent.SafeBundle;
 import com.yyxnb.android.modules.IPage;
+import com.yyxnb.android.secure.app.intent.IntentUtils;
+import com.yyxnb.android.secure.app.intent.SafeBundle;
 import com.yyxnb.android.utils.LogUtil;
 
 /**
@@ -23,51 +23,51 @@ import com.yyxnb.android.utils.LogUtil;
  */
 public class FragmentPageImpl implements IPage {
 
-	private static final String TAG = FragmentPageImpl.class.getSimpleName();
+    private static final String TAG = FragmentPageImpl.class.getSimpleName();
 
-	/**
-	 * 启动Fragment，通过容器加载
-	 *
-	 * @param fragment Fragment
-	 */
-	@Override
-	public void startFragment(Fragment fragment) {
-		startFragment(fragment, null);
-	}
+    /**
+     * 启动Fragment，通过容器加载
+     *
+     * @param fragment Fragment
+     */
+    @Override
+    public void startFragment(Fragment fragment) {
+        startFragment(fragment, null);
+    }
 
-	/**
-	 * 启动Fragment，通过容器加载
-	 *
-	 * @param fragment Fragment
-	 * @param bundle   Bundle
-	 */
-	@Override
-	public void startFragment(Fragment fragment, Bundle bundle) {
-		startFragmentForResult(fragment, bundle, 0);
-	}
+    /**
+     * 启动Fragment，通过容器加载
+     *
+     * @param fragment Fragment
+     * @param bundle   Bundle
+     */
+    @Override
+    public void startFragment(Fragment fragment, Bundle bundle) {
+        startFragmentForResult(fragment, bundle, 0);
+    }
 
-	/**
-	 * 启动Fragment，通过容器加载
-	 *
-	 * @param fragment    Fragment
-	 * @param bundle      Bundle
-	 * @param requestCode requestCode
-	 */
-	@Override
-	public void startFragmentForResult(Fragment fragment, Bundle bundle, int requestCode) {
-		try {
-			final Activity activity = ActivityUtil.getTopActivity();
-			Intent intent = new Intent(activity, ContainerActivity.class);
-			Bundle iBundle = new SafeBundle(bundle).getBundle();
-			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			intent.putExtra(ArgumentKeys.FRAGMENT, fragment.getClass().getCanonicalName());
-			intent.putExtra(ArgumentKeys.BUNDLE, iBundle);
-			boolean forResultStatic = IntentUtils.safeStartActivityForResultStatic(activity, intent, requestCode, iBundle);
-			if (!forResultStatic) {
-				activity.startActivityForResult(intent, requestCode, bundle);
-			}
-		} catch (Exception e) {
-			LogUtil.e(TAG, e.getMessage());
-		}
-	}
+    /**
+     * 启动Fragment，通过容器加载
+     *
+     * @param fragment    Fragment
+     * @param bundle      Bundle
+     * @param requestCode requestCode
+     */
+    @Override
+    public void startFragmentForResult(Fragment fragment, Bundle bundle, int requestCode) {
+        try {
+            final Activity activity = ActivityUtil.getTopActivity();
+            Intent intent = new Intent(activity, ContainerActivity.class);
+            Bundle iBundle = new SafeBundle(bundle).getBundle();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(ArgumentKeys.FRAGMENT, fragment.getClass().getCanonicalName());
+            intent.putExtra(ArgumentKeys.BUNDLE, iBundle);
+            boolean forResultStatic = IntentUtils.safeStartActivityForResultStatic(activity, intent, requestCode, iBundle);
+            if (!forResultStatic) {
+                activity.startActivityForResult(intent, requestCode, bundle);
+            }
+        } catch (Exception e) {
+            LogUtil.e(TAG, e.getMessage());
+        }
+    }
 }
